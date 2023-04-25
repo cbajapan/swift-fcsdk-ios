9416f055fa8ce8537d25f613b7a1ffd65a700e00
cba-githubdeploy <cba-adoservconn@cba-japan.com>
2023-04-25 07:20:50 +0000

    Azure Pipelines: Add new version of package checksum

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "CBARealTime"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.1.xcframework.zip", checksum: "748ab1eba5585f064ea105cab5401a4f1dc5b9ee1dbc0d9229dc75ffeae446d9"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.1.xcframework.zip", checksum: "5d9e0f83482b9776cea732714305a436973eb22a30862192513bbbc072134277"),
         .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m110-1.0.0.xcframework.zip", checksum: "a2f4cee24ce4389aa00feb86edd8dc8c67a43aedf1f8b4ceb5998c94f16a5e3d")
     ]
 )

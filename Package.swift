84d0cbba530fe51b665bb474d27ec44b070cd390
Cole Moore <Cartisim@icloud.com>
2023-07-10 22:15:50 +0400

    Updated checksum in preparation for release 4.2.4

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.3.xcframework.zip", checksum: "a6b7a2e7518f4d3fb6955ac8af372fa864895ee2e35dbc716a55f75f8c6d3a62"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.4.xcframework.zip", checksum: "b3b8691daed6c6621017f8d4e6df9a1ae6d5b0ed8417bef35298efbabbb036e0"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )

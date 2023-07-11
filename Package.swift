edc54c847a62dd0e94c47bb803bfcbba5f438f7b
Cole Moore <Cartisim@icloud.com>
2023-07-11 09:29:58 +0400

    Added Cocoapod and updated checksum for version 4.2.4

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.4.xcframework.zip", checksum: "b3b8691daed6c6621017f8d4e6df9a1ae6d5b0ed8417bef35298efbabbb036e0"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.4.xcframework.zip", checksum: "1622c4879dc2795fbb44165b10501faad2cc090038648db3805731b75336b27e"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )

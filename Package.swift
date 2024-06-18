55821d28eb5e4c224f6e29680eb531ac4bfa01b6
cba-githubdeploy <cba-adoservconn@cba-japan.com>
2024-06-18 14:19:07 +0000

    Azure Pipelines: Add new version of package checksum

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.3.0-beta1.xcframework.zip", checksum: "6df330fa955adf320153e0cf92913a8896272c1baa20fd74dc3f54667578968f"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.3.0.xcframework.zip", checksum: "991573da7a3a8091f11dca619a3f35ec485cfad919c372d3a8f0416b2f189236"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m122.xcframework.zip", checksum: "fdb0715a4d916f0cedf049d90954105cc18cad3c7189b6ef274cabb9a1bcb5d7")
     ]
 )

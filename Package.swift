73632d0e008c4c16f11a728248ef0cdd8fb5e2c4
Cole Moore <Cartisim@icloud.com>
2024-09-17 20:39:16 +0800

    Updated Checksum/Cocoapod - 4.3.4-beta1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.3.3.xcframework.zip", checksum: "6d1bee42d42789b155a0e9074678da7d96037fa6e057bbceddd753ad6887d441"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.3.4-beta1.xcframework.zip", checksum: "1d01e9b6593ca0af2fc158b3917834232fbc06f23eeafb268a43742e05529e94"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m122.xcframework.zip", checksum: "fdb0715a4d916f0cedf049d90954105cc18cad3c7189b6ef274cabb9a1bcb5d7")
     ]
 )

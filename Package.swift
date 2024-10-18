6c829b1edecfa967f2784c097dc0e1fe051e91c5
Cole Moore <Cartisim@icloud.com>
2024-10-18 19:50:43 +0800

    Binary Update for 4.3.4-beta6

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.3.4-beta5.xcframework.zip", checksum: "4797e724434e3e37cb78ab22ac0fbdbcf1d6e59b016d2eaf74a8dff5365d073f"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.3.4-beta6.xcframework.zip", checksum: "c3a96ce97cb1e3e40c1a22c64a92ed50296a2686f366169c17113b56da48e61c"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m122.xcframework.zip", checksum: "fdb0715a4d916f0cedf049d90954105cc18cad3c7189b6ef274cabb9a1bcb5d7")
     ]
 )

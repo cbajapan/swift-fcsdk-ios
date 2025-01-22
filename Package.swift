6c54333941d99b3c44ed2307675340c1a1de29ae
Cole Moore <Cartisim@icloud.com>
2025-01-22 15:48:24 +0800

    Updated WebRTC Version

--- Package.swift
+++ Package.swift
@@ -21,6 +21,6 @@ let package = Package(
                 "WebRTC"
             ]),
         .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.3.5-beta2.xcframework.zip", checksum: "721c55880a6ff329ea3bdc2f521c1ac93de8ae00158e6c80e547a6516183464b"),
-        .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m122.xcframework.zip", checksum: "fdb0715a4d916f0cedf049d90954105cc18cad3c7189b6ef274cabb9a1bcb5d7")
+        .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m130.xcframework.zip", checksum: "b6ba196509dc0a2217f7a940beb1236e29c2f4f7a5cbd51dfdd892ccae0c9f41")
     ]
 )
